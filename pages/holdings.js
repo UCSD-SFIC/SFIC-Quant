066d321115beb4c975b499df8b9a38142a913ffd
James Zhang <jjz002@ucsd.edu>
2023-11-20 10:01:03 -0800

    Add blank files for all pages

---
+++ pages/holdings.js
@@ -0,0 +1,7 @@
+export default function Holdings() {
+  return (
+    <div>
+      Current Holdings
+    </div>
+  )
+}
---
+++ pages/past-holdings.js
@@ -0,0 +1,7 @@
+export default function PastHoldings() {
+  return (
+    <div>
+      Past Holdings
+    </div>
+  )
+}
