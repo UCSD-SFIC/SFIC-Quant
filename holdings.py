ae19da63c699e982f57b79e74c5b90f14426fcd7
levsah <122415893+levsah@users.noreply.github.com>
2024-01-31 15:16:51 -0800

    add firebase config

---
+++ holdings.py
@@ -0,0 +1,22 @@
+## import the firebase or pyrebase stuff LEARN THIS SHIT FIRST 
+## 
+
+def get_all_holdings():
+    ...
+    return 
+
+def createHoldings():
+    ...
+    return 
+
+def getHoldings(ticker):
+    ...
+    return 
+
+def updateHoldings(ticker):
+    ...
+    return 
+
+def deleteHoldings(ticker):
+    ...
+    return 
