c6c275e97b8ccd27d1aa7784d5d9365148a94494
levsah <122415893+levsah@users.noreply.github.com>
2024-02-15 19:00:12 -0800

    updated getAll holdings

--- backend/editHoldings/editHolding.py
+++ backend/editHoldings/editHolding.py
@@ -9,7 +9,8 @@ ca = certifi.where()
 
 uri = os.getenv("uri")
 client = MongoClient(uri, server_api=ServerApi('1'),tlsCAFile=ca)
-
+db = client.Holdings
+holdings = db.Holdings
 ##Rememeber to install all the right packages before working
 
 
@@ -27,33 +28,32 @@ client = MongoClient(uri, server_api=ServerApi('1'),tlsCAFile=ca)
 
 
  #create one or more new holdings. Refer to schema from Github Issue Task 1: Holdings Schema #10
-# def createHoldings(): 
-#      print("Hello World!")
+def createHoldings(): 
+     print("Hello World!")
 
 
-# #get one or more holdings by ticker
-# def getHoldings(holdings):
-#   holdingsStrings = []
-#   for holding in holdings:
-#     //holding = holdings_ref.document(holding).get()
-#     //holdingsStrings.append(holding.to_dict())
-#   return holdingsStrings    
-# #get all holdings
-    
-# def getAllHoldings(): 
-#      holdingsStrings = []
-#      for doc in docs:
-#             holdingsStrings.append(f"{doc.id} => {doc.to_dict()}");
-#      return holdingsStrings
+#get one or more holdings by ticker
+def getHoldings(holdings):
+  holdingsStrings = []
+  for holding in holdings:
+    holding = holdings_ref.document(holding).get()
+    holdingsStrings.append(holding.to_dict())
+  return holdingsStrings   
 
-# #update one or more holdings by ticker   
-# def updateHoldings(): 
-#   print("!")
+#get all holdings
+...
+def getAllHoldings():
+    for holding in holdings.find():
+       print(holding)
+...
 
-# # remove one or more holdings by ticker
-# def deleteHoldings():
-#       print("Hello World!")
-
+#update one or more holdings by ticker   
+def updateHoldings(): 
+  print("!")
+...
+# remove one or more holdings by ticker
+def deleteHoldings():
+      print("Hello World!")
 
 ##Test prints in here.
 def main():
@@ -62,6 +62,8 @@ def main():
     print("Pinged your deployment. You successfully connected to MongoDB!")
   except Exception as e:
       print(e)
+  
+  getAllHoldings()
 
 if __name__ == "__main__":
     main()
