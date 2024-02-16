bd7cf51c8a4e0f6237f30e2d3187f28258744313
Divy Gobiraj <divygobi@gmail.com>
2024-02-15 18:22:47 -0800

    connected to mongo

--- backend/editHoldings/editHolding.py
+++ backend/editHoldings/editHolding.py
@@ -1,9 +1,14 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
 from dotenv import load_dotenv
+import os
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+load_dotenv()
+import certifi
+ca = certifi.where()
 
-load_dotenv('backend/.env')
+
+uri = os.getenv("uri")
+client = MongoClient(uri, server_api=ServerApi('1'),tlsCAFile=ca)
 
 ##Rememeber to install all the right packages before working
 
@@ -17,52 +22,46 @@ load_dotenv('backend/.env')
 ## UNCOMMENT THIS AND SET IT to the THE PRIVATE KEY CONFIG that you download, itll be a json file
 # PATH_TO_FB_CONFIG_BACKEND='YOUR PATH TO CONFIG GOES HERE'
 
-cred = credentials.Certificate(PATH_TO_FB_CONFIG_BACKEND)
 
-app = firebase_admin.initialize_app(cred)
 
-db = firestore.client()
-holdings_ref = db.collection("holdings")
-docs = holdings_ref.stream()
 
 
  #create one or more new holdings. Refer to schema from Github Issue Task 1: Holdings Schema #10
-def createHoldings(): 
-     print("Hello World!")
+# def createHoldings(): 
+#      print("Hello World!")
 
 
-#get one or more holdings by ticker
-def getHoldings(holdings):
-  holdingsStrings = []
-  for holding in holdings:
-    holding = holdings_ref.document(holding).get()
-    holdingsStrings.append(holding.to_dict())
-  return holdingsStrings    
-#get all holdings
+# #get one or more holdings by ticker
+# def getHoldings(holdings):
+#   holdingsStrings = []
+#   for holding in holdings:
+#     //holding = holdings_ref.document(holding).get()
+#     //holdingsStrings.append(holding.to_dict())
+#   return holdingsStrings    
+# #get all holdings
     
-def getAllHoldings(): 
-     holdingsStrings = []
-     for doc in docs:
-            holdingsStrings.append(f"{doc.id} => {doc.to_dict()}");
-     return holdingsStrings
+# def getAllHoldings(): 
+#      holdingsStrings = []
+#      for doc in docs:
+#             holdingsStrings.append(f"{doc.id} => {doc.to_dict()}");
+#      return holdingsStrings
 
-#update one or more holdings by ticker   
-def updateHoldings(): 
-  print("!")
+# #update one or more holdings by ticker   
+# def updateHoldings(): 
+#   print("!")
 
-# remove one or more holdings by ticker
-def deleteHoldings():
-      print("Hello World!")
+# # remove one or more holdings by ticker
+# def deleteHoldings():
+#       print("Hello World!")
 
 
 ##Test prints in here.
 def main():
-    #print("Running tests:")
-    #print("getAllHoldings():\n")
-    #print(getAllHoldings())
-    print("getHoldings():")
-    holdings = ["TST"]
-    print(getHoldings(holdings))
+  try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+  except Exception as e:
+      print(e)
 
 if __name__ == "__main__":
     main()
