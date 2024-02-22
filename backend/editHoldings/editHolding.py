e38c49646515fa89a1b65f6fb1721f212eb0ba33
Divy Gobiraj <divygobi@gmail.com>
2024-02-21 21:00:50 -0800

    add basic versions of api methods

--- backend/editHoldings/editHolding.py
+++ backend/editHoldings/editHolding.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from typing import List, Dict, Any
 load_dotenv()
 import certifi
 ca = certifi.where()
@@ -9,8 +10,7 @@ ca = certifi.where()
 
 uri = os.getenv("uri")
 client = MongoClient(uri, server_api=ServerApi('1'),tlsCAFile=ca)
-db = client.Holdings
-holdings = db.Holdings
+
 ##Rememeber to install all the right packages before working
 
 
@@ -24,26 +24,50 @@ holdings = db.Holdings
 # PATH_TO_FB_CONFIG_BACKEND='YOUR PATH TO CONFIG GOES HERE'
 
 
-
-
-
  #create one or more new holdings. Refer to schema from Github Issue Task 1: Holdings Schema #10
-def createHoldings(): 
-     print("Hello World!")
+
+db = client.Holdings
+holdings = db.Holdings
+def createHolding(name: str, current_quantity: int, reinvest_dividend: bool, security_type: str, ticker: str, transactions: List[Dict[str, Any]]): 
+    formatted_transactions = []
+
+    ##wouldn't need it for now but probably will need when
+    for transaction in transactions:
+        formatted_transaction = {
+            "change": transaction.get("change"),
+            "date": transaction.get("date"),
+            "price": transaction.get("price"),
+            "transaction_type": transaction.get("transaction_type")
+        }
+        formatted_transactions.append(formatted_transaction)
+    
+    holding = {
+        "Name": name,
+        "current_quantity": current_quantity,
+        "reinvest_dividend": reinvest_dividend,
+        "security_type": security_type,
+        "ticker": ticker,
+        "transactions": formatted_transactions,
+        "active": True
+    }
+    
+    # Insert the document into the collection
+    result = holdings.insert_one(holding)
+    return result.inserted_id
 
 
 #get one or more holdings by ticker
-def getHoldings(holdings):
-  holdingsStrings = []
-  for holding in holdings:
-    holding = holdings_ref.document(holding).get()
-    holdingsStrings.append(holding.to_dict())
-  return holdingsStrings   
+def getHoldings(holdingNames):
+    for holdingName in holdingNames:
+      query = holdings.find_one({"Name": holdingName})
+      if "active" in query and query["active"]:
+        print(query)
 
 #get all holdings
 ...
 def getAllHoldings():
     for holding in holdings.find():
+      if "active" in holding and holding["active"]:
        print(holding)
 ...
 
@@ -52,8 +76,15 @@ def updateHoldings():
   print("!")
 ...
 # remove one or more holdings by ticker
-def deleteHoldings():
-      print("Hello World!")
+def deleteHoldings(holdingsToDelete):
+    for holdingName in holdingsToDelete:
+      query = {"Name": holdingName}
+      result = holdings.update_one(query, {"$set":{"active": False}})
+
+def reActivateHoldings(holdingsToReactivate):
+    for holdingName in holdingsToReactivate:
+      query = {"Name": holdingName}
+      result = holdings.update_one(query, {"$set":{"active": True}})
 
 ##Test prints in here.
 def main():
@@ -63,7 +94,9 @@ def main():
   except Exception as e:
       print(e)
   
-  getAllHoldings()
+  #getAllHoldings()
+  deleteHoldings(["GOOGL_TST"])
+  getHoldings(["GOOGL_TST", "APPL_TST"])
 
 if __name__ == "__main__":
     main()
