f2cc27f7e21340ffe977cdeff26d873520124196
James Zhang <jjz002@ucsd.edu>
2024-04-06 16:11:37 -0700

    Extract data from Table Component

--- frontend/components/holdings-table.tsx
+++ frontend/components/holdings-table.tsx
@@ -12,7 +12,6 @@ import TableSortLabel from "@mui/material/TableSortLabel";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
-import Checkbox from "@mui/material/Checkbox";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -25,71 +24,7 @@ import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
-
-interface Data {
-  id: number;
-  ticker: string;
-  quantity: number;
-  security_type: string;
-  market_value: number;
-  initial_value: number;
-  change: number;
-  day_change: number;
-  cost: number;
-  exposure_percentage: number;
-  pnl: number;
-}
-
-function createData(
-  id: number,
-  ticker: string,
-  quantity: number,
-  security_type: string,
-  market_value: number,
-  initial_value: number,
-  change: number,
-  day_change: number,
-  exposure: number
-): Data {
-  const pnl = (market_value - initial_value) * quantity;
-  const cost = initial_value * quantity;
-  const exposure_percentage = 100 * exposure;
-  return {
-    id,
-    ticker,
-    quantity,
-    security_type,
-    market_value,
-    initial_value,
-    change,
-    day_change,
-    cost,
-    exposure_percentage,
-    pnl,
-  };
-}
-
-const rows = [
-  //   id ticker quantity security_type market_value initial_value change day_change exposure
-
-  // assuming change is total change in share price, and inital value is cost basis per share, then i notice 2 things
-  // 1. change is a redundant parameter to pass in. (simply market_value - initial_value) (other redundants too)
-
-  // 2. THE BUG IN SORTING IS FROM THE IMPOSSIBLE DATA. the previous example data supposed impossible situation such as initial 120, market 100, and change and daily change of 120. (given that the market is 100, daily change <=100, change <=120-100)
-
-  // With real data this would not be a problem.
-
-  // TODO: remove clutter functions and import them back in as needed
-  // api/fetch new data pipeline: fetch data -> clean data -> pass data into createData (restructure for easier access?)
-
-  //replaced with mathematically possible values:
-  //we now see sorting w/ negatives works as intended
-  //and our pnl matches up with quantity * change in price
-  createData(1, "AAPL", 169, "EQ", 100, 120, -20, 20, 0.5),
-  createData(2, "GOOG", 200, "EQ", 120, 116, 4, -10, 0.15),
-  createData(3, "META", 139, "EQ", 90, 67, 23, 23, 0.35),
-  createData(4, "BR S&P 500", 99, "ETF", 256, 50, 206, -10, 0.35),
-];
+import { rows, Data } from "/constants/data";
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
   if (b[orderBy] < a[orderBy]) {
--- frontend/components/topbar.tsx
+++ frontend/components/topbar.tsx
@@ -1,17 +1,10 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import IconButton from '@mui/material/IconButton';
-import { Icon } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu'; // Import an icon from @mui/icons-material
-import Favicon from 'react-favicon';
-import logo from './logo.png';
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 const TopBar: React.FC = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -26,12 +19,20 @@ const TopBar: React.FC = () => {
 
   return (
     <AppBar position="static">
-      <Toolbar style={{ justifyContent: 'space-between' }}>
+      <Toolbar style={{ justifyContent: "space-between" }}>
         <Button color="inherit" aria-label="SFIC" onClick={handleClose}>
-          <img src={logo.src} alt="SFIC Logo" style={{ height: '40px', marginRight: '10px', marginBottom: '10px'}} /> 
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          <img
+            src="/logo.png"
+            alt="SFIC Logo"
+            style={{
+              height: "40px",
+              marginRight: "10px",
+              marginBottom: "10px",
+            }}
+          />
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
             SFIC
-        </Typography>
+          </Typography>
         </Button>
       </Toolbar>
     </AppBar>
